5691a53fea3ea8a6e5ecfba93d05af019f0a72f0
Thomas Bak <tombak98@gmail.com>
2023-01-31 11:00:51 -0500

    skills added to seed

--- script/seed.js
+++ script/seed.js
@@ -13,7 +13,12 @@ async function seed() {
       class: 'Berserker',
       deck: 'BBBAQ',
       atk: 10000,
-      hp: 9000
+      hp: 9000,
+      skills: [
+        'Anti-Evil (Unique) EX:Increase Own ATK (3 Turns). Apply Special Attack [Enemies with debuffs (excluding unremovable debuffs)] (3 Turns)',
+        'Clairvoyance (Transcendant EX):Increase your Buster Card Start Gather Rate (3 turns). Increase your NP Gauge.',
+        'Souls Light EX:Apply Guts to self (1 time, 3 turns). Recover own HP every turn (3 turns).'
+      ]
     })
 
     await Card.create({
@@ -22,7 +27,12 @@ async function seed() {
       class: 'Caster',
       deck: 'AAABQ',
       atk: 8000,
-      hp: 10000
+      hp: 10000,
+      skills: [
+        'Dreamlike Charisma A: Increases party attack for 3 turns. Charges party NP gauge by 20%',
+        'Illusion A: Grants party Invincibiility for 1 turn. Increases party crit star gen for 1 turn. Reduces all enemies crit attack chance for 3 turns.',
+        'Hero Creation EX: Increases one allys Buster performance for 3 turns. Increases their Max HP for 3 turns. Increases their crit damage for 1 turn.'
+      ]
     })
     
     console.log("🌱🌱  Seeding Successful  🌱🌱");
